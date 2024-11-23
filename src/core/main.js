e6fe40bfd882b2a7e50f6a557bc3377fb2928e02
scook5570 <cooksm@merrimack.edu>
2024-11-23 14:25:37 -0500

    Added a different song and fixed autoplay issue

--- src/core/main.js
+++ src/core/main.js
@@ -1,5 +1,5 @@
-import * as THREE from 'three';
-import SceneManager from '/src/core/SceneManager.js';
+import * as THREE from "three";
+import SceneManager from "/src/core/SceneManager.js";
 import Stats from "three/examples/jsm/libs/stats.module.js";
 
 /* Main class which handles animating the scene */
@@ -17,19 +17,22 @@ class Main {
 
         this.animate = this.animate.bind(this);
 
+        this.animate();
+
         const listener = new THREE.AudioListener();
-    
+
         const sound = new THREE.Audio(listener);
         const audioLoader = new THREE.AudioLoader();
-    
-        audioLoader.load('public/assets/music/The Superfools - Christmas with The Superfools.mp3', function(buffer) {
-            sound.setBuffer(buffer);
-            sound.setLoop(true);
-            sound.setVolume(0.5);
-            sound.play();
-        });
 
-        this.animate();
+        audioLoader.load(
+            "public/assets/music/Dee Yan-Key - jazzy bells  (jingle bells).mp3",
+            function (buffer) {
+                sound.setBuffer(buffer);
+                sound.setLoop(true);
+                sound.setVolume(0.5);
+                sound.play();
+            }
+        );
     }
 
     // Animation loop
@@ -38,7 +41,10 @@ class Main {
 
         this.sceneManager.update(deltaTime);
 
-        this.sceneManager.renderer.render(this.sceneManager.scene, this.sceneManager.camera);
+        this.sceneManager.renderer.render(
+            this.sceneManager.scene,
+            this.sceneManager.camera
+        );
 
         this.stats.update();
 
